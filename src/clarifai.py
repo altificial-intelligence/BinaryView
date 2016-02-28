d7b695feb09770d6e2f4179752d5fb6b28d79984
Michael Baldwin <baldwmic@mail.gvsu.edu>
2016-02-27 19:17:14 -0500

    getting list of classes

--- src/clarifai.py
+++ src/clarifai.py
@@ -1,4 +1,5 @@
 from clarifai.client import ClarifaiApi
+import os
 
 class Classify:
 
@@ -8,13 +9,21 @@ class Classify:
     def classify(self, url):
         return self.clarifai.tag_image_urls(url)
 
+    def parseClassification(self, response):
+        return response['results'][0]['result']['tag']['classes'][0]
 
 def main():
-    c = Classify()
+
+
     fileName = 'https://usatelections.files.wordpress.com/2015/06/xxx_a13_trump_2000_24.jpg?w=198&h=300'
     fileName2 = 'http://politicks.org/IMAGES/CANDIDATES/2016/PRESIDENT/Donald-Trump.gif'
-    res = c.classify(fileName2)
-    print res
+    # c = Classify()
+    # classes = c.parseClassification(c.classify(fileName2))
+    # classes =  res['results'][0]['result']['tag']['classes'][0]
+    """
+    for className in classes:
+        print className
+    """
 
 if __name__ == '__main__':
     main()
